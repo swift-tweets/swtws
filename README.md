4291d8b18e33c60d6e61269a77abed8eda7ab243
Yuta Koshizawa <koher@koherent.org>
2017-01-13 01:03:03 +0900

    Support `--resolve-image` and `--resolve-code`

--- README.md
+++ README.md
@@ -49,6 +49,54 @@ swift build --clean dist
 swift build
 ```
 
+## How to get tokens
+
+`swtws` を使って Twitter や Gist に投稿するためには各種 token を取得する必要があります。
+
+### Twitter
+
+次の四つが必要です。
+
+- Consumer Key
+- Consumer Secret
+- Access Token
+- Access Token Secret
+
+取得方法については下記ページが参考になります。
+
+- [TWITTERでOAUTH認証を行う(1：TWITTERへのアプリケーション登録)](http://techbooster.org/android/mashup/4525/)
+- [Tokens from dev.twitter.com](https://dev.twitter.com/oauth/overview/application-owner-access-tokens)
+
+### GitHub
+
+Personal Access Token が必要です。下記ページの手順で取得可能です。
+
+- [Creating an access token for command-line use](https://help.github.com/articles/creating-an-access-token-for-command-line-use/)
+
+## How to upload resources in advance
+
+プレゼンテーション本番での技術的な失敗を可能な限り減らすために、事前にリソースをアップロードしておくことをオススメします。
+
+### Post codes to Gist
+
+`--resolve-code` オプションを使って tw ファイルに記述されたコードを事前に Gist に投稿することができます。 Gist に投稿するには `--github` オプションで GitHub の [Personal Access Token](https://github.com/settings/tokens) を指定する必要があります。下記の例では `ffffffffffffffffffffffffffffffffffffffff` が Access Token に当たります。
+
+tw ファイル中のコードをリンクと画像に置き換えた結果が標準出力に書き出されるので、 `> path/to/output.tw` のようにして結果を保存して下さい。
+
+```bash
+swtws path/to/tweets.tw --resolve-code --github ffffffffffffffffffffffffffffffffffffffff > path/to/output.tw
+```
+
+### Upload images to Twitter
+
+`--resolve-image` オプションを使って tw ファイルに記述された画像を事前に Twitter にアップロードすることができます。 Twitter にアップロードするには `--twitter` オプションで [Consumer Key, Consumer Secret, Access Token, Access Token Secret](https://dev.twitter.com/oauth/overview/single-user) を `,` 区切りで指定する必要があります。下記の例ではそれぞれ `WWWWWWWWWWWWWWWWWWWW`, `XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`, `YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY`, `ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ` に当たります。
+
+tw ファイル中のコードをリンクと画像に置き換えた結果が標準出力に書き出されるので、 `> path/to/output.tw` のようにして結果を保存して下さい。
+
+```bash
+swtws path/to/tweets.tw --resolve-image --twitter WWWWWWWWWWWWWWWWWWWW,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX,YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY,ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ > path/to/output.tw
+```
+
 ## Dependencies
 
 依存関係は Swift Package Manager によって自動的に解決されます。
--- Sources/main.swift
+++ Sources/main.swift
@@ -10,26 +10,60 @@ extension Array {
 enum OptionName: String {
     case h = "-h"
     case help = "--help"
+    case resolveImage = "--resolve-image"
+    case resolveCode = "--resolve-code"
+    case presentation = "--presentation"
     case c = "-c"
     case count = "--count"
+    case length = "--length"
     case twitter = "--twitter"
     case qiita = "--qiita"
     case github = "--github"
-    case presentation = "--post"
 }
 
 enum Option {
     case help
+    case resolveImage
+    case resolveCode
+    case presentation
     case count
-    case twitter(path: String)
-    case qiita(path: String)
-    case github(path: String)
-    case post
+    case length
+    case twitter(credential: OAuthCredential)
+    case qiita(token: String)
+    case github(token: String)
+}
+
+extension Option: Equatable {
+    static func ==(lhs: Option, rhs: Option) -> Bool {
+        switch (lhs, rhs) {
+        case (.help, .help):
+            return true
+        case (.resolveImage, .resolveImage):
+            return true
+        case (.resolveCode, .resolveCode):
+            return true
+        case (.presentation, .presentation):
+            return true
+        case (.count, .count):
+            return true
+        case (.length, .length):
+            return true
+        case let (.twitter(credential1), .twitter(credential2)):
+            return credential1 == credential2
+        case let (.github(token1), .github(token2)):
+            return token1 == token2
+        case let (.qiita(token1), .qiita(token2)):
+            return token1 == token2
+        case (_, _):
+            return false
+        }
+    }
 }
 
 enum ParseError: Error {
     case lackOfArgument(OptionName)
     case illegalOption(String)
+    case illegalArgumentFormat(OptionName, String)
 }
 
 func parse<S: Sequence>(_ arguments: S) throws -> ([String], [Option]) where S.Iterator.Element == String {
@@ -51,25 +85,46 @@ func parse<S: Sequence>(_ arguments: S) throws -> ([String], [Option]) where S.I
             switch optionName {
             case .h, .help:
                 options.append(.help)
+            case .resolveImage:
+                options.append(.resolveImage)
+            case .resolveCode:
+                options.append(.resolveCode)
+            case .presentation:
+                options.append(.presentation)
             case .c, .count:
                 options.append(.count)
-            case .presentation:
-                options.append(.post)
+            case .length:
+                options.append(.length)
             case .twitter:
                 guard let argument = iterator.next() else {
                     throw ParseError.lackOfArgument(optionName)
                 }
-                options.append(.twitter(path: argument))
+                let arguments = argument.components(separatedBy: ",")
+                guard arguments.count == 4 else {
+                    throw ParseError.illegalArgumentFormat(optionName, argument)
+                }
+
+                let consumerKey = arguments[0]
+                let consumerSecret = arguments[1]
+                let oauthToken = arguments[2]
+                let oauthTokenSecret = arguments[3]
+                    
+                options.append(.twitter(credential: OAuthCredential(
+                    consumerKey: consumerKey,
+                    consumerSecret: consumerSecret,
+                    oauthToken: oauthToken,
+                    oauthTokenSecret: oauthTokenSecret
+                )))
             case .qiita:
                 guard let argument = iterator.next() else {
                     throw ParseError.lackOfArgument(optionName)
                 }
-                options.append(.qiita(path: argument))
+                options.append(.qiita(token: argument))
             case .github:
                 guard let argument = iterator.next() else {
                     throw ParseError.lackOfArgument(optionName)
                 }
-                options.append(.github(path: argument))
+                options.append(.github(token: argument))
             }
         }
     }
@@ -82,13 +137,14 @@ enum CommandError: Error {
     case multipleInputs([String])
     case noSuchFile(path: String)
     case illegalEncoding(path: String)
+    case lackOfGithubToken
 }
 
 func command(inputs: [String], options: [Option]) throws {
     if let option = options.first {
         switch option {
         case .help:
-            printHelp()
+            printHelp { print($0) }
             return
         default:
             break
@@ -105,35 +161,67 @@ func command(inputs: [String], options: [Option]) throws {
         throw CommandError.illegalEncoding(path: input)
     }
     
-    let tweets = try Tweet.tweets(from: string, hashTag: "#swtws")
-
-    if options.contains(where: {
-        switch $0 {
-        case .count:
-            return true
-        default:
-            return false
-        }
-    }) {
+    var tweets = try Tweet.tweets(from: string, hashTag: "#swtws")
+    
+    if options.contains(.count) {
         printCount(of: tweets)
     } else {
-        print(tweets.map { "[\($0.length)]\n\n" + $0.description }.joined(separator: "\n\n---\n\n"))
+        let speaker = createSpeaker(with: options)
+        if options.contains(.resolveCode) {
+            tweets = try sync(operation: speaker.resolveCodes)(tweets)
+        }
+        if options.contains(.resolveImage) {
+            tweets = try sync(operation: speaker.resolveImages)(tweets)
+        }
+        
+        let displaysLengths = options.contains(.length)
+        
+        print(tweets.map {
+            if displaysLengths {
+                return "[\($0.length)]\n\n" + $0.description
+            } else {
+                return $0.description
+            }
+        }.joined(separator: "\n\n---\n\n"))
     }
 }
 
-func printHelp() {
+func printHelp(_ print: (String) -> ()) {
     print("OVERVIEW: Command line tool for Swift Tweets")
-    print()
+    print("")
     print("USAGE: swtws [-h | --help]")
-    print("             [[-c | --count] <input>]")
-    print()
+    print("             [[-c | --count] [--length] <input>]")
+    print("             [--resolve-image --twitter <oauth-credential> <input>]")
+    print("                 <oauth-credential> = <consumer-key>,<consumer-secret>,<oauth-token>,<oauth-token-secret>")
+    print("             [--resolve-code --github <access-token> <input>]")
+    print("")
     print("OPTIONS:")
     print("  -c, --count            Display counts of tweets")
     print("  -h, --help             Display this document")
+    print("  --length               Display the lengths of tweets")
+    print("  -resolve-code          Upload codes to Gist and replace them with links and Gist IDs")
+    print("  -resolve-image         Upload images to Twitter and replace them with media IDs")
 }
 
 func printCount(of tweets: [Tweet]) {
     print(tweets.count)
+}
+
+func createSpeaker(with options: [Option]) -> Speaker {
+    let twitterCredential: OAuthCredential? = options.flatMap { option in
+        guard case let .twitter(credential) = option else {
+            return nil
+        }
+        return credential
+        }.last
+    let githubToken: String? = options.flatMap { option in
+        guard case let .github(token) = option else {
+            return nil
+        }
+        return token
+        }.last
+    
+    return Speaker(twitterCredential: twitterCredential, githubToken: githubToken)
 }
 
 func main(_ arguments: [String]) {
@@ -141,9 +229,11 @@ func main(_ arguments: [String]) {
         let (inputs, options) = try parse(arguments.tail)
         try command(inputs: inputs, options: options)
     } catch let error {
-        print(error)
-        print()
-        printHelp()
+        fputs("ERROR: \(error)\n", stderr)
+        fputs("\n", stderr)
+        fputs("================================================================\n", stderr)
+        fputs("\n", stderr)
+        printHelp { fputs("\($0)\n", stderr) }
     }
 }
 
